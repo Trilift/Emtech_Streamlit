0288d8161f0993138918de9b15ab49c2849a0599
Nikko Denila <142887004+Trilift@users.noreply.github.com>
2024-05-17 21:21:38 +0800

    Update app.py

--- app.py
+++ app.py
@@ -28,7 +28,13 @@ def preprocess_image(image):
     return image_array
 
 # Streamlit app
-st.title('MNIST Digit Recognition')
+st.title('Digit Recognition Algorithm')
+st.image("number.jpg", width=800)
+st.write('This application is created by Paulo Abregrunda and Nikko Denila from CPE32S5')
+st.write('''This application can determine what number is shown in the image uploaded.
+        \n These numbers can range from 0-9.''')
+st.text('Upload your image below')
+
 
 uploaded_image = st.file_uploader("Upload an image", type=["jpg", "png"])
 
